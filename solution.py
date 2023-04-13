4a76d8064dce6aa9a82e115b077cfd309bd2ce52
Egor Chernov <90514666+EgorChernov5@users.noreply.github.com>
2023-04-13 12:02:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,4 @@ chat_id = 1107380406
 def solution(control_sample, test_sample) -> bool:
     t, p = ttest_ind(test_sample, control_sample)
     alpha = 0.02
-    if p < alpha:
-        return True
-    return False
+    return p < alpha
